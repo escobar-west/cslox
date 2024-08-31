fc3c51f969c8c7528505f4bf5592ab69f13fac31
escobar-west <victorv.suriel@gmail.com>
2024-08-31 14:51:21 -0400

    initial commit

---
+++ Interpreter/Machine.cs
@@ -0,0 +1,21 @@
+﻿namespace Interpreter;
+
+public class Machine{
+    public void runFile(string path) {
+        string text = System.IO.File.ReadAllText(path);
+        run(text);
+    }
+
+    public void runPrompt() {
+        while (true) {
+            Console.Write("> ");
+            string? line = Console.ReadLine();
+            if (line is null) break;
+            run(line);
+        }
+    }
+
+    void run(string source) {
+        Console.WriteLine(source);
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,15 @@
+﻿using Interpreter;
+
+class Program {
+    static void Main(string[] args) {
+        Machine shell = new Machine();
+        if (args.Length > 1) {
+            Console.WriteLine("Usage: jlox [script]");
+            Environment.Exit(1);
+        } else if (args.Length == 1) {
+            shell.runFile(args[0]);
+        } else {
+            shell.runPrompt();
+        }
+    }
+}
